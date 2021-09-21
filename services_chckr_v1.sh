dfd9c45c85b925c7ff8b8f28328057fa3a18b7f8
Ariel Piccolo <47399439+arielpiccolo@users.noreply.github.com>
2021-09-21 16:12:56 +0100

    Update services_chckr_v1.sh

--- services_chckr_v1.sh
+++ services_chckr_v1.sh
@@ -16,7 +16,7 @@ TAGS='__ENTER_TAGS_DIR_/tags'
 
 SLACK= ENTER SLACK CHANNEL INTEGRATION DETAILS
 
-
+#########################################################################################
 
 
 for i in "${SERVICES[@]}"
@@ -24,18 +24,18 @@ for i in "${SERVICES[@]}"
          `pgrep $i >/dev/null 2>&1`
          STATS=$(echo $?)
 
-         if  [[ $STATS == 0 ]] && [[ -f "$TAGS$i"  ]]
+         if  [[ $STATS == 0 ]] && [[ -f "$TAGS/$i"  ]]
          then
                  sleep 2
 
-         elif [[ $STATS == 0  ]] && [[ ! -f "$TAGS$i"  ]]
+         elif [[ $STATS == 0  ]] && [[ ! -f "$TAGS/$i"  ]]
          then
-                 touch $TAGS$i
+                 touch $TAGS/$i
                  MESSAGE_TAG="## Linux Srvs Chkr ## >>  $i was up and no tag was in place. -  A tag for $i was created $(hostname) $(date) "
-                 curl -X POST -H 'Content-type: application/json' --data '{ "text": "'"${i}"'", "text": "'"${MESSAGE_TAG}"'" }' $SLACK
+                 curl -X POST -H 'Content-type: application/json' --data '{ "text": "'"${i}"'", "text": "'"${MESSAGE_TAG}"'" }' $A
                  sleep 2
 
-         elif [[ $STATS == 1  ]] && [[ ! -f "$TAGS$i"   ]]
+         elif [[ $STATS == 1  ]] && [[ ! -f "$TAGS/$i"   ]]
          then
                 sleep 2
                 systemctl restart $i
@@ -45,12 +45,12 @@ for i in "${SERVICES[@]}"
                 if [[ $RESTART == 0  ]]
                 then
                         MESSAGE_UP="## Linux Srvs Chkr ## >>  $i was down, but I was able to restart it on Server >> $(hostname) $(date)  "
-                        curl -X POST -H 'Content-type: application/json' --data '{ "text": "'"${i}"'", "text": "'"${MESSAGE_UP}"'" }' $SLACK
+                        curl -X POST -H 'Content-type: application/json' --data '{ "text": "'"${i}"'", "text": "'"${MESSAGE_UP}"'" }' $A
                         touch $TAGS$i
                         sleep 2
                 fi
 
-        elif [[ $STATS == 1  ]] && [[ -f "$TAGS$i"  ]]
+        elif [[ $STATS == 1  ]] && [[ -f "$TAGS/$i"  ]]
         then
                 systemctl restart $i
                 `pgrep $i >/dev/null 2>&1`
@@ -59,15 +59,16 @@ for i in "${SERVICES[@]}"
                 if  [[ $RESTART == 0  ]]
                 then
                         MESSAGE_UP="## Linux Srvs Chkr ## >>  $i was down, but I was able to restart it on Server >> $(hostname) $(date)  "
-                        curl -X POST -H 'Content-type: application/json' --data '{ "text": "'"${i}"'", "text": "'"${MESSAGE_UP}"'" }' $SLACK
+                        curl -X POST -H 'Content-type: application/json' --data '{ "text": "'"${i}"'", "text": "'"${MESSAGE_UP}"'" }' $A
                         sleep 2
                 else
                         MESSAGE_DOWN="## Linux Srvs Chkr ##  >>  $i is down on Server >> $(hostname)  at $(date)  "
-                        rm $TAGS$i
-                        curl -X POST -H 'Content-type: application/json' --data '{ "text": "'"${i}"'", "text": "'"${MESSAGE_DOWN}"'" }' $SLACK
+                        rm $TAGS/$i
+                        curl -X POST -H 'Content-type: application/json' --data '{ "text": "'"${i}"'", "text": "'"${MESSAGE_DOWN}"'" }' $A
                         sleep 2
                 fi
          fi
  done
 exit 0;
+
 
