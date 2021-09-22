f8ca30a21e9ca92bc00cfdbe6959e7c3339bb621
Ariel Piccolo <47399439+arielpiccolo@users.noreply.github.com>
2021-09-22 14:22:19 +0100

    update
    small update.
    No passing param -f to pgrep to catch the -full service name

--- services_chckr_v1.sh
+++ services_chckr_v1.sh
@@ -22,7 +22,7 @@ TAGS='/root/Linux_server_services_checker_v1-main/tags'
 
 for i in "${SERVICES[@]}"
  do
-         `pgrep $i >/dev/null 2>&1`
+         `pgrep -f $i >/dev/null 2>&1`
          STATS=$(echo $?)
 
          if  [[ $STATS == 0 ]] && [[ -f "$TAGS/$i"  ]]
@@ -40,21 +40,21 @@ for i in "${SERVICES[@]}"
          then
                 sleep 2
                 systemctl restart $i
-                `pgrep $i >/dev/null 2>&1`
+                `pgrep -f $i >/dev/null 2>&1`
                 RESTART=$(echo $?)
 
                 if [[ $RESTART == 0  ]]
                 then
                         MESSAGE_UP="## Linux Srvs Chkr ## >>  $i was down, but I was able to restart it on Server >> $(hostname) $(date)  "
                         curl -X POST -H 'Content-type: application/json' --data '{ "text": "'"${i}"'", "text": "'"${MESSAGE_UP}"' <!channel>", "link_names" : 1 }' $SLACK
-                        touch $TAGS$i
+                        touch $TAGS/$i
                         sleep 2
                 fi
 
         elif [[ $STATS == 1  ]] && [[ -f "$TAGS/$i"  ]]
         then
                 systemctl restart $i
-                `pgrep $i >/dev/null 2>&1`
+                `pgrep -f $i >/dev/null 2>&1`
                 RESTART=$(echo $?)
 
                 if  [[ $RESTART == 0  ]]
